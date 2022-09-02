bb2c36117ad5aa43c6147d59a05dd06bee077cca
bal7hazar <bal7hazar@proton.me>
2022-09-02 11:15:09 +0200

    🐛 iteration

--- scripts/starknet.sh
+++ scripts/starknet.sh
@@ -48,8 +48,6 @@ wait_for_acceptance() {
 send_transaction() {
     transaction=$1
     network=$2
-    echo $transaction
-    echo $network
     while true
     do
         execute $transaction || exit_error "Error when sending transaction"
