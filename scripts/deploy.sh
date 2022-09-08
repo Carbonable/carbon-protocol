440bc24968f94364f797b82acb3a951a11b445aa
bal7hazar <bal7hazar@proton.me>
2022-09-08 18:09:17 +0200

    👽️ update CI scripts - improve code capitalization - upgrade protostar to v0.3.3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,7 @@ PROTOSTAR_TOML_FILE=$ROOT/protostar.toml
 NETWORK=
 
 ### FUNCTIONS
-. $SCRIPT_DIR/logging.sh # Logging utilities
-. $SCRIPT_DIR/tools.sh   # script utilities
+. $SCRIPT_DIR/library.sh # Logging utilities
 
 # print the script usage
 usage() {
@@ -22,99 +21,6 @@ build() {
     log_info "Building project to generate latest version of the ABI"
     execute protostar build
     if [ $? -ne 0 ]; then exit_error "Problem during build"; fi
-}
-
-# get the account address from the account alias in protostar accounts file
-# $1 - account alias (optional). __default__ if not provided
-get_account_address() {
-    [ $# -eq 0 ] && account=__default__ || account=$1
-    grep $account $STARKNET_ACCOUNTS_FILE -A3 -m1 | sed -n 's@^.*"address": "\(.*\)".*$@\1@p'
-}
-
-# get the network from the profile in protostar config file
-# $1 - profile
-get_network() {
-    profile=$1
-    grep profile.$profile $PROTOSTAR_TOML_FILE -A3 -m1 | sed -n 's@^.*[network=|gateway_url=]"\(.*\)".*$@\1@p'
-}
-
-# check starknet binary presence
-check_starknet() {
-    which starknet &> /dev/null
-    [ $? -ne 0 ] && exit_error "Unable to locate starknet binary. Did you activate your virtual env ?"
-}
-
-# make sure wallet variable is set
-check_wallet() {
-    [ -z $WALLET ] && exit_error "Please provide the wallet to use (option -w or environment variable STARKNET_WALLET)"
-}
-
-# wait for a transaction to be received
-# $1 - transaction hash to check
-wait_for_acceptance() {
-    tx_hash=$1
-    if [[ "$tx_hash" != *"0x"* ]]; then
-        tx_hash=$(felt_to_hex "$1")
-    fi
-    while true 
-    do
-        tx_status=`starknet tx_status --hash $tx_hash --network $NETWORK | sed -n 's@^.*"tx_status": "\(.*\)".*$@\1@p'`
-        case "$tx_status"
-            in
-                NOT_RECEIVED|RECEIVED|PENDING) print -n  $(magenta .);;
-                REJECTED) return 1;;
-                ACCEPTED_ON_L1|ACCEPTED_ON_L2) return 0; break;;
-                *) exit_error "\nUnknown transaction status '$tx_status'";;
-            esac
-            sleep 2
-    done
-}
-
-# convert an ASCII string to felt
-# $1 - string value
-str_to_felt() {
-    str_val=$1
-    hex_bytes=$(echo $str_val | xxd -p)
-    hex_bytes=0x$(echo $hex_bytes | rev | cut -c3- | rev)
-    echo $hex_bytes
-}
-
-# convert hex to felt
-# $1 - hex value
-hex_to_felt() {
-    hex_upper=`echo ${1//0x/} | tr '[:lower:]' '[:upper:]'`
-    echo "obase=10; ibase=16; $hex_upper" | BC_LINE_LENGTH=0 bc
-}
-
-# convert felt to hex
-# $1 - felt value
-felt_to_hex() {
-    felt=`echo "obase=16; ibase=10; $1" | BC_LINE_LENGTH=0 bc`
-    echo 0x$felt
-}
-
-# send a transaction
-# $* - command line to execute
-# return The contract address
-send_transaction() {
-    transaction=$*
-
-    while true
-    do
-        execute $transaction || exit_error "Error when sending transaction"
-        
-        contract_address=`sed -n 's@Contract address: \(.*\)@\1@p' logs.json`
-        tx_hash=`sed -n 's@Transaction hash: \(.*\)@\1@p' logs.json`
-
-        [ $PROFILE != "devnet" ] && wait_for_acceptance $tx_hash || print "Acceptance skipped in $PROFILE profile"
-
-        case $? in
-            0) log_success "\nTransaction accepted!"; break;;
-            1) log_warning "\nTransaction rejected!"; ask "Do you want to retry";;
-        esac
-    done || exit_error
-
-    echo $contract_address
 }
 
 # Deploy all contracts and log the deployed addresses in the cache file
@@ -140,7 +46,7 @@ deploy_all_contracts() {
         erc721_name=$(str_to_felt "$ERC721_NAME")
         erc721_symbol=$(str_to_felt "$ERC721_SYMBOL")
         log_info "Deploying ERC-721 contract..."
-        ERC721_ADDRESS=`send_transaction "protostar $PROFILE_OPT deploy ./build/CarbonableProject.json --inputs $erc721_name $erc721_symbol $ADMIN_ADDRESS"` || exit_error
+        ERC721_ADDRESS=`send_transaction "protostar $PROFILE_OPT deploy ./build/CarbonableProject.json --inputs $erc721_name $erc721_symbol $ADMIN_ADDRESS" "$NETWORK"` || exit_error
     fi
 
     # Deploy Minter contract
@@ -148,10 +54,10 @@ deploy_all_contracts() {
         owner=$ADMIN_ADDRESS
         project_nft_address=$ERC721_ADDRESS
         log_info "Deploying Minter contract..."
-        MINTER_ADDRESS=`send_transaction "protostar $PROFILE_OPT deploy ./build/CarbonableMinter.json --inputs $owner $project_nft_address $PAYMENT_TOKEN_ADDRESS $PUBLIC_SALE_OPEN $MAX_BUY_PER_TX $UNIT_PRICE $MAX_SUPPLY_FOR_MINT $RESERVED_SUPPLY_FOR_MINT"` || exit_error
+        MINTER_ADDRESS=`send_transaction "protostar $PROFILE_OPT deploy ./build/CarbonableMinter.json --inputs $owner $project_nft_address $PAYMENT_TOKEN_ADDRESS $PUBLIC_SALE_OPEN $MAX_BUY_PER_TX $UNIT_PRICE $MAX_SUPPLY_FOR_MINT $RESERVED_SUPPLY_FOR_MINT" "$NETWORK"` || exit_error
         # Transfer ownership
         log_info "Transfer ERC-721 ontract ownership..."
-        ERC721_ADDRESS=`send_transaction "starknet invoke --address $ERC721_ADDRESS --abi ./build/CarbonableProject_abi.json --function transferOwnership --inputs $MINTER_ADDRESS --network $NETWORK --account $ACCOUNT --wallet $WALLET"` || exit_error
+        ERC721_ADDRESS=`send_transaction "starknet invoke --address $ERC721_ADDRESS --abi ./build/CarbonableProject_abi.json --function transferOwnership --inputs $MINTER_ADDRESS --network $NETWORK --account $ACCOUNT --wallet $WALLET" "$NETWORK"` || exit_error
     fi    
 
     # Save values in cache file
@@ -179,10 +85,10 @@ done
 CONFIG_FILE=$ROOT/scripts/configs/$PROFILE.config
 [ -f $CONFIG_FILE ] && source $CONFIG_FILE || exit_error "$CONFIG_FILE file not found"
 
-[ -z $ADMIN_ADDRESS ] && ADMIN_ADDRESS=`get_account_address $ACCOUNT`
+[ -z $ADMIN_ADDRESS ] && ADMIN_ADDRESS=`get_account_address $ACCOUNT $STARKNET_ACCOUNTS_FILE`
 [ -z $ADMIN_ADDRESS ] && exit_error "Unable to determine account address"
 
-[[ -z $NETWORK && ! -z $PROFILE ]] && NETWORK=`get_network $PROFILE`
+[[ -z $NETWORK && ! -z $PROFILE ]] && NETWORK=`get_network $PROFILE $PROTOSTAR_TOML_FILE`
 [ -z $NETWORK ] && exit_error "Unable to determine network"
 
 ### PRE_CONDITIONS
--- scripts/library.sh
+++ scripts/library.sh
@@ -4,4 +4,5 @@ SCRIPT_DIR=`readlink -f $0 | xargs dirname`
 source $SCRIPT_DIR/logging.sh
 source $SCRIPT_DIR/tools.sh
 source $SCRIPT_DIR/converter.sh
+source $SCRIPT_DIR/checker.sh
 source $SCRIPT_DIR/starknet.sh
