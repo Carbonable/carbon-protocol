38880c24069a42abd15d5f55e4d2cd11f3ee7748
Clement <bal7hazar@proton.me>
2022-09-09 17:29:18 +0200

    👷 deploy github actions (#52)
    * 👷 add build and deploy workflow
    
    * 💚 update CI tests trigger event
    
    * 👷 implement manual deploy workflows
    
    👷 implement manual deploy workflows
    
    * ⚰️ remove dead file
    
    * ✏️ fix typo
    
    * 👽️ update CI scripts
    - improve code capitalization
    - upgrade protostar to v0.3.3
    
    * 👽️ update CI scripts
    - improve code capitalization
    - upgrade protostar to v0.3.3
    
    * 💚  fix cairo lang version
    
    * 💚 fix typo

---
+++ scripts/converter.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# convert an ASCII string to felt
+# $1 - string value
+str_to_felt() {
+    str_val=$1
+    hex_bytes=$(echo $str_val | xxd -p)
+    hex_bytes=0x$(echo $hex_bytes | rev | cut -c3- | rev)
+    echo $hex_bytes
+}
+
+# convert hex to felt
+# $1 - hex value
+hex_to_felt() {
+    hex_upper=`echo ${1//0x/} | tr '[:lower:]' '[:upper:]'`
+    echo "obase=10; ibase=16; $hex_upper" | BC_LINE_LENGTH=0 bc
+}
+
+# convert felt to hex
+# $1 - felt value
+felt_to_hex() {
+    felt=`echo "obase=16; ibase=10; $1" | BC_LINE_LENGTH=0 bc`
+    echo 0x$felt
+}
+
+# convert felt to uint256
+# $1 - felt value
+felt_to_uint256() {
+    echo "$1 0"
+}
+
+# convert bool to felt
+# $1 - felt value
+bool_to_felt() {
+    if [ $1 = true ] ; then felt=1 ; else felt=0; fi
+    echo $felt
+}
