1fa913a4af2eae6c07a6ffab0ba34b9a57a6d4ba
bal7hazar <bal7hazar@proton.me>
2023-09-22 14:28:22 +0200

    ✨ Add min claimable feature on offseter

--- scripts/deploy_offseter.sh
+++ scripts/deploy_offseter.sh
@@ -4,6 +4,7 @@ source ../.env
 SIERRA_FILE=../target/dev/carbon_Offseter.sierra.json
 PROJECT=0x007afb15db3fb57839fec89c20754eb59f8d7e3f87d953ee68b0a99b6f527b3e
 SLOT=1
+MIN_CLAIMABLE=1
 OWNER=0x063675fa1ecea10063722e61557ed7f49ed2503d6cdd74f4b31e9770b473650c
 
 # build the contract
@@ -33,10 +34,11 @@ declare() {
 # deploy the contract
 # $1 - Project
 # $2 - Slot
-# $3 - Owner
+# $3 - Min Claimable
+# $4 - Owner
 deploy() {
     class_hash=$(declare | tail -n 1)
-    output=$(starkli deploy $class_hash "$PROJECT" u256:"$SLOT" "$OWNER" --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
+    output=$(starkli deploy $class_hash "$PROJECT" u256:"$SLOT" u256:"$MIN_CLAIMABLE" "$OWNER" --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
     
     if [[ $output == *"Error"* ]]; then
         echo "Error: $output"
--- src/components/offset/interface.cairo
+++ src/components/offset/interface.cairo
@@ -6,6 +6,8 @@ trait IOffset<TContractState> {
     fn get_total_claimed(self: @TContractState) -> u256;
     fn get_claimable_of(self: @TContractState, account: ContractAddress) -> u256;
     fn get_claimed_of(self: @TContractState, account: ContractAddress) -> u256;
+    fn get_min_claimable(self: @TContractState) -> u256;
     fn claim(ref self: TContractState, amount: u256);
     fn claim_all(ref self: TContractState);
+    fn set_min_claimable(ref self: TContractState, min_claimable: u256);
 }
