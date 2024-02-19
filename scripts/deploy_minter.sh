2ddce89b3132d7419f1e09b12a9df52026c54bdd
Arn0d <arnaud@carbonable.io>
2024-02-19 01:17:33 +0100

    Update deployment scripts with new configuration

--- scripts/deploy_minter.sh
+++ scripts/deploy_minter.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 source ../.env
 # Check if --debug parameter is passed
-debug="false"
-for arg in "$@"
-do
-    if [ "$arg" == "--debug" ]
-    then
-        debug="true"
-    fi
+debug=0
+
+TEMP=$(getopt -o d --long debug -- "$@")
+
+eval set -- "$TEMP"
+
+while true ; do
+    case "$1" in          
+        -d|--debug)
+            debug=1 ; shift ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
 done
 
 SIERRA_FILE=../target/dev/carbon_Minter.sierra.json
-PROJECT=0x007afb15db3fb57839fec89c20754eb59f8d7e3f87d953ee68b0a99b6f527b3e
+PROJECT=0x014c6533fec6fd168189b49150907db533e8be3a2e69b0657ae4ec6459a94668
 SLOT=1
 ERC20=0x005a643907b9a4bc6a55e9069c4fd5fd1f5c79a22470690f75556c4736e34426
-PUBLIC_SALE_OPEN=0
+PUBLIC_SALE_OPEN=1
 ENABLE_MAX_PER_TX=0
 MIN_VALUE_PER_TX=1000000
-MAX_VALUE_PER_TX=5000000
-MAX_VALUE=100000000
+MAX_VALUE_PER_TX=0
+MAX_VALUE=2139004800000
 UNIT_PRICE=1
-RESERVED_VALUE=10000000
-OWNER=0x05bB7458b87FaaA41303A69B771ae26235F28b79aBD5FA1C451C43461DFE1438
+RESERVED_VALUE=0
+OWNER=0x5bb7458b87faaa41303a69b771ae26235f28b79abd5fa1c451c43461dfe1438
 
 # build the contract
 build() {
@@ -36,7 +42,7 @@ build() {
 # declare the contract
 declare() {
     build
-    if [[ $debug == "true" ]]; then
+    if [ $debug -eq 1 ]; then
         printf "declare %s\n" "$SIERRA_FILE" > debug_minter.log
     fi
     output=$(starkli declare $SIERRA_FILE --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
@@ -71,7 +77,7 @@ deploy() {
     class_hash=$(declare)
     sleep 5    
 
-    if [[ $debug == "true" ]]; then        
+    if [ $debug -eq 1 ]; then        
         printf "deploy %s %s %s %s %s %s %s %s %s %s %s %s \n" "$class_hash" "$PROJECT" "$SLOT" "$ERC20" "$PUBLIC_SALE_OPEN" "$ENABLE_MAX_PER_TX" "$MAX_VALUE_PER_TX" "$MIN_VALUE_PER_TX" "$MAX_VALUE" "$UNIT_PRICE" "$RESERVED_VALUE" "$OWNER" >> debug_minter.log
     fi
     output=$(starkli deploy $class_hash "$PROJECT" u256:"$SLOT" "$ERC20" "$PUBLIC_SALE_OPEN" "$ENABLE_MAX_PER_TX" u256:"$MAX_VALUE_PER_TX" u256:"$MIN_VALUE_PER_TX" u256:"$MAX_VALUE" u256:"$UNIT_PRICE" u256:"$RESERVED_VALUE" "$OWNER" --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
@@ -95,7 +101,7 @@ setup() {
     contract=$(deploy)
     sleep 5
 
-    if [[ $debug == "true" ]]; then
+    if [ $debug -eq 1 ]; then
         printf "invoke %s add_minter %s %s \n" "$PROJECT" "$SLOT" "$contract" >> debug_minter.log
     fi
     output=$(starkli invoke $PROJECT add_minter u256:$SLOT $contract --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
--- scripts/deploy_project.sh
+++ scripts/deploy_project.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 source ../.env
 
-# Check if --debug parameter is passed
-debug="false"
-for arg in "$@"
-do
-    if [ "$arg" == "--debug" ]
-    then
-        debug="true"
-    fi
+# Check parameters
+debug=0
+old_network=0
+
+TEMP=$(getopt -o dg --long goerli,debug -- "$@")
+
+eval set -- "$TEMP"
+
+while true ; do
+    case "$1" in
+        -g|--goerli)
+            old_network=1 ; shift ;;            
+        -d|--debug)
+            debug=1 ; shift ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
 done
 
 SIERRA_FILE=../target/dev/carbon_Project.sierra.json
-NAME=0x436172626f6e5f54657374
+NAME="Carb_Var_Max_value"
 SYMBOL="CARBT"
 DECIMALS=6
-OWNER=0x01e2F67d8132831f210E19c5Ee0197aA134308e16F7f284bBa2c72E28FC464D2
+OWNER=0x5bb7458b87faaa41303a69b771ae26235f28b79abd5fa1c451c43461dfe1438
 SLOT=1
-PROJECT_VALUE=17600000000
-TIMES="24 1667314458 1698850458 1730472858 1762008858 1793544858 1825080858 1856703258 1888239258 1919775258 1951311258 1982933658 2046005658 2109164058 2172236058 2235394458 2266930458 2330002458 2361624858 2393160858 2424696858 2456232858 2487855258 2582463258 2614085658"
-ABSORPTIONS=" 24 0 4719000 12584000 25168000 40898000 64493000 100672000 147862000 202917000 265837000 333476000 478192000 629200000 773916000 915486000 983125000 1108965000 1164020000 1223794000 1280422000 1335477000 1387386000 1528956000 1573000000"
+PROJECT_VALUE=2139004800000
+TIMES="2 1717236000 2442996000"
+ABSORPTIONS=" 2 0 410400000000"
 TON_EQUIVALENT=1000000
 
 # build the solution
@@ -35,9 +44,10 @@ build() {
 # declare the contract
 declare() {
     build
-    if [[ $debug == "true" ]]; then
+    if [ $debug -eq 1 ]; then
         printf "declare %s\n" "$SIERRA_FILE" > debug_project.log
     fi
+
     output=$(starkli declare $SIERRA_FILE --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
 
     if [[ $output == *"Error"* ]]; then
@@ -50,7 +60,7 @@ declare() {
         address=$(echo -e "$output" | ggrep -oP '0x[0-9a-fA-F]+')
     else
         # If ggrep is not available, use grep
-        address=$(echo -e "$output" | grep -oP '0x[0-9a-fA-F]\+')
+        address=$(echo -e "$output" | grep -oP '0x[0-9a-fA-F]+')
     fi
     echo $address
 }
@@ -64,11 +74,11 @@ deploy() {
     class_hash=$(declare | tail -n 1)
     sleep 5
     
-    if [[ $debug == "true" ]]; then
+    if [ $debug -eq 1 ]; then
         printf "deploy %s %s %s %s %s \n" "$class_hash" "$NAME" "$SYMBOL" "$DECIMALS" "$OWNER" >> debug_project.log
     fi
 
-    output=$(starkli deploy $class_hash "$NAME" str:"$SYMBOL" "$DECIMALS" "$OWNER" --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
+    output=$(starkli deploy $class_hash str:"$NAME" str:"$SYMBOL" "$DECIMALS" "$OWNER" --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
 
     if [[ $output == *"Error"* ]]; then
         echo -e "Error: $output"
@@ -89,31 +99,34 @@ setup() {
     contract=$(deploy)
     sleep 5
 
-    if [[ $debug == "true" ]]; then
+    if [ $debug -eq 1 ]; then
         printf "invoke %s set_project_value %s %s \n" "$contract" "$SLOT" "$PROJECT_VALUE" >> debug_project.log
     fi
 
     output=$(starkli invoke $contract set_project_value u256:$SLOT u256:$PROJECT_VALUE --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
+
     if [[ $output == *"Error"* ]]; then
         echo -e "Error: $output"
         exit 1
     fi
 
-    if [[ $debug == "true" ]]; then
+    if [ $debug -eq 1 ]; then
         printf "invoke %s set_certifier %s %s \n" "$contract" "$SLOT" "$OWNER" >> debug_project.log
     fi
-
+    
     output=$(starkli invoke $contract set_certifier u256:$SLOT $OWNER --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
+
     if [[ $output == *"Error"* ]]; then
         echo -e "Error: $output"
         exit 1
     fi
 
-    if [[ $debug == "true" ]]; then
+    if [ $debug -eq 1 ]; then
         printf "invoke %s set_absorptions u256:%s %s %s %s \n" "$contract" "$SLOT" "$TIMES" "$ABSORPTIONS" "$TON_EQUIVALENT" >> debug_project.log
     fi
 
     output=$(starkli invoke $contract set_absorptions u256:$SLOT $TIMES $ABSORPTIONS $TON_EQUIVALENT --keystore-password $KEYSTORE_PASSWORD --watch 2>&1)
+
     if [[ $output == *"Error"* ]]; then
         echo -e "Error: $output"
         exit 1
